fcf9d032574e0fff076b792fd2eed8f0ab6f5636
Jandos <97423847+jandoskg@users.noreply.github.com>
2022-02-10 23:53:28 +0600

    Update MyNavbar.jsx

--- src/components/MyNavbar/MyNavbar.jsx
+++ src/components/MyNavbar/MyNavbar.jsx
@@ -7,7 +7,11 @@ const MyNavbar = ({ handleContacts }) => {
     <>
       <nav className="Nav">
         <div className="left-navbar-items">
-          <h3 className="nav-text">My Contact Book</h3>
+           <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkBjdPpydc7JHT6l7eW0O2dRDWLaC9-gvIeg&usqp=CAU"
+            alt="logo"
+           />
+           <h3 className="nav-text">My Contact Book</h3>
         </div>
         <div className="center-navbar-items">
           <a href="#"> More info</a>
